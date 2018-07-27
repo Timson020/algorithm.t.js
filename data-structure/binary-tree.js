9614acdeb8fce7a056ddd959d783dd89371332fe
Timson020 <a_sa_f@126.com>
2018-07-28 06:43:41 +0800

    modify the binary-tree.js

--- data-structure/binary-tree.js
+++ data-structure/binary-tree.js
@@ -22,10 +22,43 @@ class Binary {
 		if ((node.key > rootNode.key) && rootNode.right) Binary.insertNode(rootNode.right, node)
 	}
 
+	static inOrderTraversalNode(node, cb) {
+		if (!node) return
+		Binary.inOrderTraversalNode(node.left, cb)
+		cb(node.key)
+		Binary.inOrderTraversalNode(node.right, cb)
+	}
+
+	static preOrderTraversalNode(node, cb) {
+		if (!node) return
+		cb(node.key)
+		Binary.preOrderTraversalNode(node.left, cb)
+		Binary.preOrderTraversalNode(node.right, cb)
+	}
+
+	static postOrderTraversalNode(node, cb) {
+		if (!node) return
+		Binary.postOrderTraversalNode(node.left, cb)
+		Binary.postOrderTraversalNode(node.right, cb)	
+		cb(node.key)
+	}
+
 	insert(value) {
 		const node = new Node(value)
 
 		!this.tree ? this.tree = node : Binary.insertNode(this.tree, node)
+	}
+
+	inOrderTraversal(cb) {
+		Binary.inOrderTraversalNode(this.tree, cb)
+	}
+
+	preOrderTraversal(cb) {
+		Binary.preOrderTraversalNode(this.tree, cb)
+	}
+
+	postOrderTraversal(cb) {
+		Binary.postOrderTraversalNode(this.tree, cb)	
 	}
 }
 
@@ -45,6 +78,18 @@ class Binary {
 // 		!root ? root = node : insertNode(root, node)
 // 	}
 
+// 	this.inOrderTraversal = function(cb) {
+// 		inOrderTraversalNode(root, cb)
+// 	}
+
+// 	this.preOrderTraversal = function(cb) {
+// 		preOrderTraversalNode(root, cb)
+// 	}
+
+// 	this.postOrderTraversal = function(cb) {
+// 		postOrderTraversalNode(root, cb)
+// 	}
+
 // 	function insertNode(rootNode, node) {
 // 		if ((node.key < rootNode.key) && !rootNode.left) rootNode.left = node
 		
@@ -53,7 +98,27 @@ class Binary {
 // 		if ((node.key > rootNode.key) && !rootNode.right) rootNode.right = node
 
 // 		if ((node.key > rootNode.key) && rootNode.right) insertNode(rootNode.right, node)
-// 		console.info(root)
+// 	}
+
+// 	function inOrderTraversalNode(node, cb) {
+// 		if (!node) return
+// 		inOrderTraversalNode(node.left, cb)
+// 		cb(node.key)
+// 		inOrderTraversalNode(node.right, cb)
+// 	}
+
+// 	function preOrderTraversalNode(node, cb) {
+// 		if (!node) return
+// 		cb(node.key)
+// 		preOrderTraversalNode(node.left, cb)
+// 		preOrderTraversalNode(node.right, cb)	
+// 	}
+
+// 	function postOrderTraversalNode(node, cb) {
+// 		if (!node) return
+// 		postOrderTraversalNode(node.left, cb)
+// 		postOrderTraversalNode(node.right, cb)
+// 		cb(node.key)
 // 	}
 // }
 
@@ -67,8 +132,34 @@ function newDate() {
 	list.forEach(it => tree.insert(it))
 
 	console.info(tree)
+	if (global) global._tree = tree
 
-	this._tree = tree
+	doInOrderTraversal()
+	console.info('中序遍历完成')
+
+	doPreOrderTraversal()
+	console.info('前序遍历完成')
+
+	
+	doPostOrderTraversal()
+	console.info('后序遍历完成')
+}
+
+// 执行中序遍历
+function doInOrderTraversal() {
+	if (global._tree) global._tree.inOrderTraversal((value) => console.info(value))
+}
+
+// 执行前序遍历
+function doPreOrderTraversal() {
+	if (global._tree) global._tree.preOrderTraversal((value) => console.info(value))
+}
+
+// 执行后续遍历
+function doPostOrderTraversal() {
+	if (global._tree) global._tree.postOrderTraversal((value) => console.info(value))	
 }
 
 newDate()
+
+
