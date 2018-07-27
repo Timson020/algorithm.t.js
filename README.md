331e0039fca50002f26f0b4f72a7ec32336da9c9
Timson020 <a_sa_f@126.com>
2018-07-28 05:54:20 +0800

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+# 数据结构与算法（JavaScript）
+
+> 属于个人整理
+
+## 数据结构
+
+|名称|地址|描述|
+|--|--|--|
+|二叉树|[binary-tree.js](./data-structure/binary-tree.js)|排序二叉树|
+
+## 算法
---
+++ data-structure/binary-tree.js
@@ -0,0 +1,74 @@
+// ES6
+class Node {
+	constructor(value) {
+		this.key = value
+		this.right = null
+		this.left = null
+	}
+}
+
+class Binary {
+	constructor() {
+		this.tree = null
+	}
+
+	static insertNode(rootNode, node) {
+		if ((node.key < rootNode.key) && !rootNode.left) rootNode.left = node
+		
+		if ((node.key < rootNode.key) && rootNode.left) Binary.insertNode(rootNode.left, node)
+
+		if ((node.key > rootNode.key) && !rootNode.right) rootNode.right = node
+
+		if ((node.key > rootNode.key) && rootNode.right) Binary.insertNode(rootNode.right, node)
+	}
+
+	insert(value) {
+		const node = new Node(value)
+
+		!this.tree ? this.tree = node : Binary.insertNode(this.tree, node)
+	}
+}
+
+const tree = new Binary()
+
+const list = [5,56,57,89,123,1,30,6,0]
+
+list.forEach(it => tree.insert(it))
+
+console.info(tree)
+
+
+// ES5
+// function Binary() {
+// 	let root = null
+	
+// 	const Node = function(value) {
+// 		this.key = value
+// 		this.left = null
+// 		this.right = null
+// 	}
+
+// 	this.insert = function(value) {
+// 		const node = new Node(value)
+// 		!root ? root = node : insertNode(root, node)
+// 	}
+
+// 	function insertNode(rootNode, node) {
+// 		if ((node.key < rootNode.key) && !rootNode.left) rootNode.left = node
+		
+// 		if ((node.key < rootNode.key) && rootNode.left) insertNode(rootNode.left, node)
+
+// 		if ((node.key > rootNode.key) && !rootNode.right) rootNode.right = node
+
+// 		if ((node.key > rootNode.key) && rootNode.right) insertNode(rootNode.right, node)
+// 		console.info(root)
+// 	}
+// }
+
+// const tree = new Binary()
+
+// const list = [5,56,57,89,123,1,30,6,0]
+
+// list.forEach(it => tree.insert(it))
+
+// console.info(tree)
